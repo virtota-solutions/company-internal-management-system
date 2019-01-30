7d294a224620a76dedc3a1625cece2eccaf2f0d0
cjhorton19 <30810541+cjhorton19@users.noreply.github.com>
2019-01-30 14:02:09 -0800

    Created ASP.NET Web Application (.NET Framework) and Angular Application
    Web API with windows authentification.
    Angular routing is not used

---
+++ SeniorProjectAPI/SeniorProjectAPI/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace SeniorProjectAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
